427163cdcb0f7ea97f5e12395c39b662ab858367
mmaruf23 <maruuf82@gmail.com>
2024-12-23 22:35:10 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const jam = document.querySelectorAll(".jarum-jam");
+const menit = document.querySelectorAll(".jarum-menit");
+const detik = document.querySelectorAll(".jarum-detik");
+const rotatejam = document.querySelector(".jam");
+const rotatemenit = document.querySelector(".menit");
+const rotatedetik = document.querySelector(".detik");
+
+function init() {
+  minutes = new Date().getMinutes();
+  rotatemenit.style.rotate = `${minutes * 6}deg`;
+  menit.forEach(function (e) {
+    e.textContent = minutes;
+    e.style.rotate = `-${minutes * 6}deg`;
+  });
+  hours = new Date().getHours();
+  rotatejam.style.rotate = `${hours * 15}deg`;
+
+  jam.forEach(function (e) {
+    e.textContent = hours;
+    e.style.rotate = `-${hours * 15}deg`;
+  });
+}
+init();
+
+setInterval(() => {
+  const now = new Date();
+  const seconds = now.getSeconds();
+
+  rotatedetik.style.rotate = `${seconds * 6}deg`;
+  detik.forEach(function (e) {
+    e.textContent = seconds;
+    e.style.rotate = `-${seconds * 6}deg`;
+  });
+
+  if (seconds === 0) {
+    minutes = now.getMinutes();
+    rotatemenit.style.rotate = `${minutes * 6}deg`;
+    menit.forEach(function (e) {
+      e.textContent = minutes;
+      e.style.rotate = `-${minutes * 6}deg`;
+    });
+  }
+
+  if (seconds === 0 && now.getMinutes() === 0) {
+    hours = now.getHours();
+    rotatejam.style.rotate = `${hours * 15}deg`;
+
+    jam.forEach(function (e) {
+      e.textContent = hours;
+      e.style.rotate = `-${hours * 15}deg`;
+    });
+  }
+}, 1000);
